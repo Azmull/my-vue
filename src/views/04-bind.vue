40d2854e40a54ac42391d6e7871d023dae84d498
Shime <s95187463@gmail.com>
2025-12-26 09:04:43 +0800

    理解到04-資料綁定

--- src/views/04-bind.vue
+++ src/views/04-bind.vue
@@ -1,27 +1,23 @@
 <script setup>
-import{ref} from 'vue'
-const data = ref("test")
-
-
+import { ref } from 'vue'
+const data = ref('test')
 </script>
 
 <template>
   <h2>04-資料綁定</h2>
   <div>
-    <h2> 單向綁定data:{{ data }}</h2>
+    <h2>單向綁定data:{{ data }}</h2>
   </div>
   <div>
     1單向綁定 v-bind:value
-    <input v-bind:value="data">
+    <input v-bind:value="data" />
   </div>
   <div>
     2單向綁定 :value
-    <input :value="data">
+    <input :value="data" />
   </div>
-  <br>
   <div>
     3雙向綁定 v-model
-    <input v-model="data">
+    <input v-model="data" />
   </div>
-
 </template>
