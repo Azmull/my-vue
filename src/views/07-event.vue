151916626239af495e9a29d40226d5f9883600d3
Shime <s95187463@gmail.com>
2025-12-27 15:01:25 +0800

    複習完07-事件綁定

--- src/views/07-event.vue
+++ src/views/07-event.vue
@@ -1,40 +1,37 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
 
 const num = ref(0)
 const count = ref()
 const keyword = ref('')
 
-function increment(){
-  num.value+=1
+function increment() {
+  num.value += 1
 }
-function countChange(){
+function countChange() {
   num.value = count.value
 }
-function clickonce(){
+function clickonce() {
   alert('只會顯示一次')
 }
-function search(){
+function search() {
   alert(keyword.value)
 }
-function f8(){
-  alert("你按下F8")
+function f8() {
+  alert('你按下F8')
 }
 </script>
 
 <template>
   <h2>07-事件綁定</h2>
-  <div>
-    現在的數值:{{num}}
-  </div>
+  <div>現在的數值:{{ num }}</div>
   <button v-on:click="increment()">按我+1</button>
-  <br>
+  <br />
   <button @click="increment()">++</button>
-  <br>
-  <input type="number" v-model="count" @change="countChange()">
-  <br>
+  <br />
+  <input type="number" v-model="count" @change="countChange()" />
+  <br />
   <button @click.once="clickonce">按一次就失效</button>
-  <br>
-  搜尋: <input v-model="keyword" @keyup.enter="search()">
-  F8: <input @keyup.f8="f8()">
+  <br />
+  搜尋: <input v-model="keyword" @keyup.enter="search()" /> F8: <input @keyup.f8="f8()" />
 </template>
--- src/views/08-computed.vue
+++ src/views/08-computed.vue
@@ -1,32 +1,29 @@
 <script setup>
-import { ref, reactive, computed } from 'vue';
+import { ref, reactive, computed } from 'vue'
 
 // 類別清單
 const categories = [
-{ id: "f1", name: '水果' },
-{ id: "f2", name: '蔬菜' },
-{ id: "f3", name: '飲料' }
+  { id: 'f1', name: '水果' },
+  { id: 'f2', name: '蔬菜' },
+  { id: 'f3', name: '飲料' },
 ]
 
 // 商品清單
 const items = [
-{ id: 1, name: '蘋果', categoryId: 'f1' },
-{ id: 2, name: '香蕉', categoryId: 'f1' },
-{ id: 3, name: '胡蘿蔔', categoryId: 'f2' },
-{ id: 4, name: '青花菜', categoryId: 'f2' },
-{ id: 5, name: '可樂', categoryId: 'f3' },
-{ id: 6, name: '柳橙汁', categoryId: 'f3' }
+  { id: 1, name: '蘋果', categoryId: 'f1' },
+  { id: 2, name: '香蕉', categoryId: 'f1' },
+  { id: 3, name: '胡蘿蔔', categoryId: 'f2' },
+  { id: 4, name: '青花菜', categoryId: 'f2' },
+  { id: 5, name: '可樂', categoryId: 'f3' },
+  { id: 6, name: '柳橙汁', categoryId: 'f3' },
 ]
 
 const selectedCategory = ref('')
 
-const filteredComputed = computed(()=>{
-  if(selectedCategory.value!="")
-  {
-    return items.filter(i=>i.categoryId==selectedCategory.value)
-  }
-  else
-  {
+const filteredComputed = computed(() => {
+  if (selectedCategory.value != '') {
+    return items.filter((i) => i.categoryId == selectedCategory.value)
+  } else {
     return items //回傳全部項目
     // return [] //回傳空陣列
   }
@@ -44,7 +41,7 @@ const filteredComputed = computed(()=>{
       </option>
     </select>
     商品:
-    <select >
+    <select>
       <option v-for="item in filteredComputed">
         {{ item.id }} {{ item.name }} {{ item.categoryId }}
       </option>
