e8dd39b8da4e6f27fa0a979dc66da79842d66b21
Shime <s95187463@gmail.com>
2025-12-28 14:47:17 +0800

    複習完11-全域變數

--- src/views/11-env.vue
+++ src/views/11-env.vue
@@ -1,44 +1,43 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from 'vue'
 
 console.log(window.js_global_title)
 const jstitle = ref('')
 jstitle.value = window.js_global_title
 
-const envTitle = ref('')
-envTitle.value = import.meta.env.VITE_TITLE
-const envTitle2 = ref('')
-envTitle2.value = import.meta.env.VITE_TITLE2
-
+const enva = ref('')
+enva.value = import.meta.env.VITE_a
+const envb = ref('')
+envb.value = import.meta.env.VITE_b
+const envc = ref('')
+envc.value = import.meta.env.VITE_c
 const propertyTitle = ref('')
-import {getCurrentInstance} from 'vue'
-const {proxy}=getCurrentInstance() //解構寫法
+import { getCurrentInstance } from 'vue'
+const { proxy } = getCurrentInstance() //解構寫法
 // propertyTitle.value = proxy.$propertyTitle
 propertyTitle.value = proxy.$propertyTitle
 
-import { inject } from 'vue';
+import { inject } from 'vue'
 const provideTitle = ref('')
 provideTitle.value = inject('provideTitle')
-
 </script>
 
-
 <template>
-
   <h2>11-全域變數</h2>
   <div>{{ jstitle }}</div>
-  <br>
+  <br />
   <div>
-    {{ envTitle }}
-    <br>
-    {{ envTitle2 }}
+    {{ enva }}
+    <br />
+    {{ envb }}
+    <br />
+    {{ envc }}
   </div>
-  <br>
+  <br />
   <div>
     {{ propertyTitle }}
   </div>
   <div>
     {{ provideTitle }}
   </div>
-
 </template>
